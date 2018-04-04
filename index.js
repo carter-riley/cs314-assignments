22d392833e9940c1b1aad895cb3dea844201a454
carter-riley <carter.j.riley@outlook.com>
2018-04-04 16:52:52 -0700

    finished final changes

--- index.js
+++ index.js
@@ -2,6 +2,14 @@
 
 // 1: Char Swap
 function charSwap(inputString) {
+	if(inputString == null) {
+		alert("You hit cancel");
+		return;
+	} else if(inputString == "") {
+		alert("You did not enter anything");
+		return;
+	}
+
 	let first = inputString.slice(0,1);
 	let last = inputString.slice(inputString.length - 1, inputString.length);
 	
@@ -15,6 +23,14 @@ function charSwap(inputString) {
 
 // 2: Hi String
 function greeting(name) {
+	if(name == null) {
+		alert("You hit cancel");
+		return;
+	} else if(name == "") {
+		alert("You did not enter anything");
+		return;
+	}
+
 	if(name.substring(0,2) == "Hi") {
 		alert(name);
 	} else {
@@ -24,12 +40,30 @@ function greeting(name) {
 
 // 3: Three Chars to Front
 function threeCharFront(inputString) {
+	if(inputString == null) {
+		alert("You hit cancel");
+		return;
+	} else if(inputString == "") {
+		alert("You did not enter anything");
+		return;
+	} else if(inputString.length < 3) {
+		alert("The string was shorter than three characters.");
+		return;
+	}
+
 	let newString = inputString.slice(inputString.length - 3,inputString.length) + inputString.substring(0,inputString.length - 3);
 	alert("The new string is: " + newString);
 }
 
 // 4: Strings to Sentence
 function stringSentence(stringList) {
+	if(stringList == null) {
+		alert("You hit cancel");
+		return;
+	} else if(stringList == "") {
+		alert("You did not enter anything");
+		return;
+	}
 	let stringArray = stringList.split(", ");
 	
 	alert(`My favorite color is ${stringArray[0]}, ${stringArray[1]} are awesome, and I love ${stringArray[2]}`);
@@ -37,6 +71,13 @@ function stringSentence(stringList) {
 
 // 5: Upper or Lower
 function upperLower(someString) {
+	if(someString == null) {
+		alert("You hit cancel");
+		return;
+	} else if(someString == "") {
+		alert("You did not enter anything");
+		return;
+	}
 	let newString = "";
 	
 	if(someString.length > 3) {
@@ -51,6 +92,14 @@ function upperLower(someString) {
 
 // 6: Integer Swap
 function integerSwap(numList) {
+	if(numList == null) {
+		alert("You hit cancel");
+		return;
+	} else if(numList == "") {
+		alert("You did not enter anything");
+		return;
+	}
+
 	let numArray = numList.split(",");
 	
 	let last = numArray.pop();
@@ -63,6 +112,14 @@ function integerSwap(numList) {
 
 // 7: Longest String
 function longestString(stringList){
+	if(stringList == null) {
+		alert("You hit cancel");
+		return;
+	} else if(stringList == "") {
+		alert("You did not enter anything");
+		return;
+	}
+
 	let stringArray = stringList.split(",");
 	let lengthOfLongestString = 0;
 	let currLongestString = 0;
@@ -80,8 +137,10 @@ function longestString(stringList){
 function largestEvenNumber(numList) {
 	if(numList == null) {
 		alert("You hit cancel");
+		return;
 	} else if(numList == "") {
 		alert("You didn\'t input anything.");
+		return;
 	}
 
 	let largestNum = 0;
@@ -157,13 +216,13 @@ function unlimitedFunction() {
 }
 
 // Function calls:
-// charSwap(prompt("Please enter a string."));
-// greeting(prompt("Please enter a string."));
-// threeCharFront(prompt("Please enter a string longer than 3 characters"));
-// stringSentence(prompt("Please enter strings seperated by commas."));
-// upperLower(prompt("Please input a string."));
-// integerSwap(prompt("Please enter numbers seperated by commas."));
-// longestString(prompt("Please enter strings seperated by commas."));
-// largestEvenNumber(prompt("Please enter numbers seperated by commas."));
-// currentDayTime();
-// unlimitedFunction("Hello ", "my ", "name ", "is ", "Carter.");
+charSwap(prompt("Please enter a string."));
+greeting(prompt("Please enter a string."));
+threeCharFront(prompt("Please enter a string longer than 3 characters"));
+stringSentence(prompt("Please enter strings seperated by commas."));
+upperLower(prompt("Please input a string."));
+integerSwap(prompt("Please enter numbers seperated by commas."));
+longestString(prompt("Please enter strings seperated by commas."));
+largestEvenNumber(prompt("Please enter numbers seperated by commas."));
+currentDayTime();
+unlimitedFunction("Hello ", "my ", "name ", "is ", "Carter.");
